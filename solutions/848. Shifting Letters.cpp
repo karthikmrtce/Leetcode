79dc6a4f9bac19daf5a64a01f84deb37a14e3d10
karthikmrtce <70596109+karthikmrtce@users.noreply.github.com>
2021-09-09 11:32:58 +0530

    197 ms

---
+++ solutions/848. Shifting Letters.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+       long long  sum=0;
+       for(int i=s.size()-1;i>=0;i--)
+       {
+          
+           sum=(sum+shifts[i])%26;
+           s[i]=(s[i]-'a'+sum)%26+'a';
+       }
+     
+      return s;
+    }
+};
